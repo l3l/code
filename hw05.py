f63bf8c98ca99d81d1aa95757a4187561763848e
l3l <dun02170@gmail.com>
2017-06-13 00:06:54 +0900

    red black tree를 이용하여 해당 file의 name, total node 수, insert한 횟수, delete한 횟수, delete하려 했으나 하지 못한 횟수, black node의 수, black height의 수와 inorder traversal의 결과를 출력하는 코드이다.

--- hw05.py
+++ hw05.py
@@ -6,7 +6,6 @@ class Node:
 
     def __init__(self, newval):
         self.val = newval
-        # self.color = 'RED'
         self.left = None
         self.right = None
         self.parent = None
